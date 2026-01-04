2b61ddecee118014575bc9ff7b467834a1e59e8e
OmkarArdekar12 <omkarardekar12@gmail.com>
2026-01-04 17:21:20 +0530

    Registration

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,82 @@
+"use client";
+import { useActionState } from "react";
+import { registerAction } from "@/app/actions/auth";
+import Link from "next/link";
+
+const RegisterPage = () => {
+  const [state, formAction] = useActionState(registerAction, {});
+
+  return (
+    <div className="max-w-md mx-auto bg-white p-8 rounded-lg shadow-lg">
+      <h1 className="text-2xl font-bold mb-6">Register</h1>
+
+      <form action={formAction} className="space-x-4">
+        <div className="w-full">
+          <label
+            htmlFor="name"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Name
+          </label>
+          <input
+            id="name"
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            required
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm p-2"
+          />
+        </div>
+        <div className="w-full mt-3">
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+            required
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm p-2"
+          />
+        </div>
+        <div className="w-full mt-3">
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Password
+          </label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+            required
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm p-2"
+          />
+        </div>
+        {state?.error && (
+          <p className="text-red-500 text-sm my-1">{state.error}</p>
+        )}
+        <button
+          type="submit"
+          className="mt-3 w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 transition-colors cursor-pointer"
+        >
+          Register
+        </button>
+      </form>
+      <p className="mt-4 text-center">
+        Already have an account ?{" "}
+        <Link href="/login" className="text-blue-600 hover:underline">
+          Login
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default RegisterPage;
