125af1db673ce9f7a618d2eafe2e32e814c4f2ad
OmkarArdekar12 <omkarardekar12@gmail.com>
2026-01-04 17:45:59 +0530

    Login Form

--- src/app/_components/LoginForm.tsx
+++ src/app/_components/LoginForm.tsx
@@ -1,10 +1,12 @@
 "use client";
-import React from "react";
+import React, { useActionState } from "react";
 import { loginAction } from "../actions/auth";
 
 const LoginForm = () => {
+  const [state, formAction] = useActionState(loginAction, {});
+
   return (
-    <form action={loginAction} className="space-x-4">
+    <form action={formAction} className="space-x-4">
       <div className="w-full">
         <label
           htmlFor="email"
@@ -37,9 +39,12 @@ const LoginForm = () => {
           className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm p-2"
         />
       </div>
+      {state?.error && (
+        <p className="text-sm text-red-600 my-1">{state.error}</p>
+      )}
       <button
         type="submit"
-        className="mt-3 w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 transition-colors cursor-pointer"
+        className="mt-5 w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 transition-colors cursor-pointer"
       >
         Login
       </button>
