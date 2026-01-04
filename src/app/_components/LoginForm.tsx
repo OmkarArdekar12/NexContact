116134d590836ea05705987fe9f1facc26cc0de6
OmkarArdekar12 <omkarardekar12@gmail.com>
2026-01-04 17:16:30 +0530

    Login Form

--- src/app/_components/LoginForm.tsx
+++ src/app/_components/LoginForm.tsx
@@ -1,7 +1,50 @@
+"use client";
 import React from "react";
+import { loginAction } from "../actions/auth";
 
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  return (
+    <form action={loginAction} className="space-x-4">
+      <div className="w-full">
+        <label
+          htmlFor="email"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Email
+        </label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          placeholder="Enter your email"
+          required
+          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm p-2"
+        />
+      </div>
+      <div className="w-full mt-3">
+        <label
+          htmlFor="password"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Password
+        </label>
+        <input
+          id="password"
+          type="password"
+          name="password"
+          placeholder="Enter your password"
+          required
+          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm p-2"
+        />
+      </div>
+      <button
+        type="submit"
+        className="mt-3 w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 transition-colors cursor-pointer"
+      >
+        Login
+      </button>
+    </form>
+  );
 };
 
 export default LoginForm;
