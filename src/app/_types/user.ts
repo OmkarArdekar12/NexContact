6f2823525c59fe9dca8ca13d73aab70bd073b09a
OmkarArdekar12 <omkarardekar12@gmail.com>
2026-01-04 17:19:28 +0530

    User Type

---
+++ src/app/_types/user.ts
@@ -0,0 +1,12 @@
+export interface UserType {
+  id: string;
+  email: string;
+  name: string;
+}
+
+export type RegisterUserType = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+};
