a2e1e7eb297357d5918316f8014871e99ba58579
OmkarArdekar12 <omkarardekar12@gmail.com>
2026-01-04 17:20:38 +0530

    Login Page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,20 @@
+import LoginForm from "@/app/_components/LoginForm";
+import Link from "next/link";
 import React from "react";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="max-w-md mx-auto bg-white p-8 rounded-lg shadow-lg">
+      <h1 className="text-2xl font-bold mb-6">Login</h1>
+      <LoginForm />
+      <p className="mt-4 text-center">
+        Don't have an account ?{" "}
+        <Link href="/register" className="text-blue-600 hover:underline">
+          Register
+        </Link>
+      </p>
+    </div>
+  );
 };
 
 export default LoginPage;
