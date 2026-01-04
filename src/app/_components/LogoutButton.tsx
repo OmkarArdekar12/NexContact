a0045400f817ab86447a141c8592883eec7f2857
OmkarArdekar12 <omkarardekar12@gmail.com>
2026-01-04 17:17:05 +0530

    Logout

---
+++ src/app/_components/LogoutButton.tsx
@@ -0,0 +1,28 @@
+"use client";
+import React from "react";
+import { logoutAction } from "../actions/auth";
+import { useRouter } from "next/navigation";
+
+const LogoutButton = () => {
+  const router = useRouter();
+  const handleLogout = async () => {
+    try {
+      await logoutAction();
+      // redirect("/login");
+      router.push("/login");
+      router.refresh();
+    } catch (err) {
+      console.log("Logout failed: ", err);
+    }
+  };
+  return (
+    <button
+      className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 transition-colors cursor-pointer"
+      onClick={handleLogout}
+    >
+      Logout
+    </button>
+  );
+};
+
+export default LogoutButton;
