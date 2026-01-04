b7932bd61c52109141cda56854385880fae3d528
OmkarArdekar12 <omkarardekar12@gmail.com>
2026-01-04 17:23:09 +0530

    Auth actions completed

---
+++ src/app/actions/auth.ts
@@ -0,0 +1,77 @@
+"use server";
+import axios from "axios";
+import { redirect } from "next/navigation";
+import { RegisterUserType, UserType } from "../_types/user";
+import { deleteSession, setSession } from "../_lib/session";
+
+const API_URL = "http://localhost:3001";
+
+type RegisterState = {
+  error?: string;
+};
+
+export const registerAction = async (
+  prevState: RegisterState,
+  formData: FormData
+): Promise<RegisterState> => {
+  const name = formData.get("name") as string;
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+
+  if (!name || !email || !password) {
+    return { error: "All fields are required" };
+  }
+
+  try {
+    const existingUser = await axios.get(`${API_URL}/users?email=${email}`);
+
+    if (existingUser.data.length > 0) {
+      return { error: "Email already registered" };
+    }
+
+    const newUser: { name: string; email: string; password: string } = {
+      name,
+      email,
+      password,
+    };
+
+    const response = await axios.post(`${API_URL}/users`, newUser);
+
+    const user: RegisterUserType = response.data;
+
+    await setSession({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } catch (error) {
+    return { error: "Registration failed. Try again." };
+  }
+
+  redirect("/contact");
+};
+
+export const loginAction = async (formData: FormData) => {
+  console.log("formData: ", formData);
+  try {
+    const response = await axios.get(
+      `${API_URL}/users?email=${formData.get("email")}&password=${formData.get(
+        "password"
+      )}`
+    );
+    const user: UserType = response.data[0];
+    if (!user) {
+      throw new Error("Invalid Credentials");
+    }
+    // set user in the cookies
+    await setSession({ name: user.name, email: user.email, id: user.id });
+  } catch (err) {
+    return { error: "Failed to login. Please try again." };
+  }
+  redirect("/contact");
+};
+
+export const logoutAction = async () => {
+  await deleteSession();
+  redirect("/login");
+};
