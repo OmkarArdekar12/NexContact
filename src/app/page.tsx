c89ba8f1ab92b3e4f963e21ddd21179e74a66562
OmkarArdekar12 <omkarardekar12@gmail.com>
2026-01-04 17:38:28 +0530

    Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,22 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-5 gap-5 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-5 gap-5 sm:p-20 font-sans">
       <div className="text-center">
-        <h1 className="text-4xl font-bold">Welcome to Contact Manager</h1>
+        <h1 className="text-4xl font-bold">Welcome to NexContact</h1>
+        <h2 className="mt-3 text-3xl font-semibold">
+          A Contact Manager Application
+        </h2>
         <p className="mt-2 text-lg text-gray-600">
           Manage your contacts easily and efficiently
         </p>
       </div>
       <Image
-        src="/contacts.png"
+        src="/logo.png"
         alt="contact manager"
         width={300}
         height={300}
-        className="rounded-lg shadow-lg"
+        className="rounded-lg drop-shadow-[1px_1px_0_rgb(0,0,0)]"
       />
       <div className="text-center">
         <p className="mt-2 text-lg text-gray-600">
